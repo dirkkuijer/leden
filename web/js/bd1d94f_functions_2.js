3e200cead1bf4f67cd0b3c61bd11b84ef4c9215a
Dirk Kuijer <dirkkuijer@hotmail.com>
2019-04-12 17:11:09 +0200

    AddForm improved and external file linking

---
+++ web/js/bd1d94f_functions_2.js
@@ -0,0 +1,20 @@
+// search field in index.html.twig
+function emptyForm() {
+    document.getElementById("zoeken").reset();
+    }
+
+
+$(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
+
+
+// flash message
+setTimeout(function() {
+    $('.flash-success').fadeOut('slow');
+    }, 3000); // <-- time in milliseconds
