bfefa6591c13905fab86d8a8defda3490c33faa1
Dirk Kuijer <dirkkuijer@hotmail.com>
2019-04-29 17:13:17 +0200

    session time and contactform

---
+++ src/AppBundle/Controller/ContactController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(Request $request, \Swift_Mailer $mailer)
+    {
+        
+        $form = $this->createForm(ContactType::class);
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) 
+        {
+            $contactFormData = $form->getData();
+            
+            $message = (new \Swift_Message('Contactformulier bmk'))
+               ->setFrom($contactFormData['email'])
+               ->setTo('baarnsmannenkoor@outlook.com')
+               ->setBody(
+                   $contactFormData['message'],
+                   'text/plain'
+               )
+           ;
+
+           $mailer->send($message);
+           
+           $state = "E-mail is verzonden.";
+           // added by Dirk
+           $this->showFlash($state);
+   
+           return $this->render('user/help.html.twig', [
+            'form' => $form->createView()
+        ]);
+           
+        } else {
+               return $this->render('contact.html.twig', [
+                   'form' => $form->createView()
+               ]);
+                $state = "Probeer het opnieuw! Verzenden mislukt.";
+                $this->showFlash($state);
+        }
+        
+    }
+
+      // added by Dirk to show flash messages after submitting form
+      public function showFlash(String $state) {
+        return $this->addFlash('success', $state);
+    }
+}
---
+++ src/AppBundle/Form/ContactType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use AppBundle\Form\ContactType;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('subject', ChoiceType::class, [
+                'multiple' => false,
+                'expanded' => false,
+                'choices' => [
+                    'Wachtwoord vergeten' => 'Wachtwoord vergeten',
+                    'Persoonsgegevens' => 'Persoonsgegevens',
+                    'Gebruikersgegevens' => 'Gebruikersgegevens',
+                    'Anders' => 'Anders'
+                ]
+            ])
+            ->add('message', TextareaType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // uncomment if you want to bind to a class
+            //'data_class' => Contact::class,
+        ]);
+    }
+}
