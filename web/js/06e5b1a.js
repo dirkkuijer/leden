e876fabfc4c65914b14cb6547adf5b75e10657ee
Dirk Kuijer <dirkkuijer@hotmail.com>
2019-07-04 12:13:11 +0200

    Testing parts

--- web/js/06e5b1a.js
+++ web/js/06e5b1a.js
@@ -10368,23 +10368,28 @@ function emptyForm() {
     document.getElementById("zoeken").reset();
     }
 
-
-$(document).ready(function(){
+    $(document).ready(function(){
   
     $("#myInput").on("keyup", function() {
         var value = $(this).val().toLowerCase();
         $("#myTable tr").filter(function() {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-
         });
     });
+;
 });
-
 
 // flash message
 setTimeout(function() {
     $('.flash-success').fadeOut('slow');
     }, 5000); // <-- time in milliseconds
+
+
+// disapear footer
+function footer() {
+    $('.footer').fadeToggle('slow');
+    $('.box-footer').fadeToggle('slow');
+}
 
 
 
